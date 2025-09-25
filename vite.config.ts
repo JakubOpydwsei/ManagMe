be19cc290c67e58de2be02cb9b6565cf5978a2c5
JakubOpyd <jakub.opyd@microsoft.wsei.edu.pl>
2025-09-25 20:18:41 +0200

    chore(tests): setup unit testing with Vitest and add test scripts

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,6 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/setupTests.ts',
+    exclude: ['E2ETests/*', 'backend/*', 'node_modules', 'dist', 'express/*'],
   }
 })
