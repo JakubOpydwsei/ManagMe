b68be688de31e0fb231ce7c4806cf183c15f28b7
JakubOpyd <jakub.opyd@microsoft.wsei.edu.pl>
2025-09-25 20:42:17 +0200

    fix(types): resolve typing issue in GoogleLogin component

--- src/components/GoogleLonig.tsx
+++ src/components/GoogleLonig.tsx
@@ -1,45 +1,44 @@
-import { useEffect } from 'react'
-import { useAuth } from '../contexts/AuthContext'
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    google: any
+    google: any;
   }
 }
 
-const CLIENT_ID = import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID
+const CLIENT_ID = import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID;
 
 const GoogleLogin = () => {
-  const { loginGoogle } = useAuth()
+  const { loginGoogle } = useAuth();
 
   useEffect(() => {
     if (!CLIENT_ID) {
-      console.error('Google Client ID is not set')
-      return
+      console.error("Google Client ID is not set");
+      return;
     }
 
     window.google?.accounts.id.initialize({
       client_id: CLIENT_ID,
       callback: handleCredentialResponse,
-    })
+    });
 
     window.google?.accounts.id.renderButton(
-      document.getElementById('googleSignInButton'),
-      { theme: 'outline', size: 'large' }
-    )
-  }, [])
-
-  const handleCredentialResponse = (response: google.accounts.id.CredentialResponse) => {
+      document.getElementById("googleSignInButton"),
+      { theme: "outline", size: "large" }
+    );
+  }, []);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleCredentialResponse = (response: any) => {
     // console.log("JWT-google:", response.credential)
-  
-    loginGoogle(response.credential).catch((error) => {
-      console.error("Google login error:", error.message)
-    })
-  }
 
-  
-  return <div className='w-fit m-auto pt-2' id="googleSignInButton"></div>
-}
+    loginGoogle(response.credential).catch((error) => {
+      console.error("Google login error:", error.message);
+    });
+  };
+
+  return <div className="w-fit m-auto pt-2" id="googleSignInButton"></div>;
+};
 
 export default GoogleLogin;
