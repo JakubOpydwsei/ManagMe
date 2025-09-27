75c4ca118c2d0257e2a71aa0ae8657b82c898490
JakubOpyd <jakub.opyd@microsoft.wsei.edu.pl>
2025-09-27 18:36:51 +0200

    test(LoginForm): add UI render tests

--- src/components/LoginForm.test.tsx
+++ src/components/LoginForm.test.tsx
@@ -1,10 +1,21 @@
 import { render, screen } from "../../tests/test-utils";
 import LoginForm from "./LoginForm";
 
-describe("LoginForm", () => {
-  it("renders the LoginForm component", () => {
+describe("LoginForm UI", () => {
+  it("renders the login button", () => {
     render(<LoginForm />);
 
+    expect(screen.getByRole("button", { name: /Login/i })).toBeInTheDocument();
+  });
+
+  it("renders inputs", () => {
+    render(<LoginForm />);
+
+    expect(
+      screen.getByText(/Login with existing account/i)
+    ).toBeInTheDocument();
+    expect(screen.getByLabelText(/Login:/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Password:/i)).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /Login/i })).toBeInTheDocument();
   });
 });
